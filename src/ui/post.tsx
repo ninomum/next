0ec53317de55511e7b175d203736b252fa3a7474
ginnylu <ginny.lu@cn.mcd.com>
2025-05-14 13:56:41 +0800

    1

--- src/ui/post.tsx
+++ src/ui/post.tsx
@@ -1,15 +1,18 @@
 import { PostType } from '@/types/post';
 import '@/styles/globals.scss';
+import Link from 'next/link'
 
 export function Post({ post }: { post: PostType }) {
     return (
         <li className="postItem">
-            <article className="postCard">
-                <header>
-                    <h2 className="postTitle">{post.title}</h2>
-                </header>
-                <p className="postContent">{post.content}</p>
-            </article>
-        </li>
+            <Link href={`/blog/${post.id}`}>
+                <article className="postCard">
+                    <header>
+                        <h2 className="postTitle">{post.title}</h2>
+                    </header>
+                    <p className="postContent">{post.content}</p>
+                </article>
+            </Link >
+        </li >
     );
 }
