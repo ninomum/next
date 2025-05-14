123724ea141c02a43e6eabdf279b7c5dc2c11849
ginnylu <ginny.lu@cn.mcd.com>
2025-05-14 14:20:17 +0800

    1

--- src/ui/post.tsx
+++ src/ui/post.tsx
@@ -6,20 +6,20 @@ import { Button } from 'antd';
 export function Post({ post }: { post: PostType }) {
     return (
         <li className="postItem">
-            <Link href={`/blog/${post.id}`}>
-                <article className="postCard">
-                    <header>
-                        <h2 className="postTitle">{post.title}</h2>
-                    </header>
+            <article className="postCard">
+                <Link href={`/blog/${post.id}`}>
+                    <h2 className="postTitle">{post.title}</h2>
                     <p className="postContent">{post.content}</p>
-                </article>
-            </Link >
-            <Link href={`/blog/${post.id}/edit`}>
-                <Button type="primary">Edit</Button>
-            </Link>
-            <Link href={`/blog/${post.id}/history`}>
-                <Button type="primary">History</Button>
-            </Link>
+                </Link >
+                <div className="postActions">
+                    <Link href={`/blog/${post.id}/edit`}>
+                        <Button type="primary">Edit</Button>
+                    </Link>
+                    <Link href={`/blog/${post.id}/history`}>
+                        <Button type="primary">History</Button>
+                    </Link>
+                </div>
+            </article>
         </li >
     );
 }
