e55e4cd5fcb7194d24ee3764d4cd94985fa0a70d
ginnylu <ginny.lu@cn.mcd.com>
2025-05-29 13:37:27 +0800

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <p>home</p>
       </body>
     </html>
   );
