e0b38f2a2a8b7267cbdf17672aa1f92b0a10077d
ginnylu <ginny.lu@cn.mcd.com>
2025-06-05 15:35:06 +0800

    1

--- src/app/blog/[id]/edit/page.tsx
+++ src/app/blog/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-export default async function EditPage({ params }: { params: { id: string } }) {
+export default async function EditPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return (
     <div>
