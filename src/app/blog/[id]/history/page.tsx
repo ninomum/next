fb07ac88447c1a57f3a1a6cccb2675e2ef2f704f
ginnylu <ginny.lu@cn.mcd.com>
2025-06-05 15:36:47 +0800

    1

--- src/app/blog/[id]/history/page.tsx
+++ src/app/blog/[id]/history/page.tsx
@@ -1,9 +1,9 @@
 
-export default function HistoryPage({ params }: { params: { id: string } }) {
+export default async function HistoryPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return (
     <div>
-      <h1>History Post {params.id}</h1>
-      {/* 编辑表单历史可以放在这里 */}
+      <h1>History Post {id}</h1>
     </div>
   );
 }
