bd91bb1c64f012934204ce7ff121da802f2ac072
ginnylu <ginny.lu@cn.mcd.com>
2025-06-05 15:09:45 +0800

    1

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,15 @@
 import type { Metadata } from 'next'
+import { Caveat } from 'next/font/google'
+
+const caveat = Caveat({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+  adjustFontFallback: true,
+  fallback: ['system-ui', 'sans-serif'],
+  weight: '700',
+})
 
 export const metadata: Metadata = {
   title: 'nino',
@@ -10,5 +21,7 @@ export default function Layout({
 }: {
   children: React.ReactNode
 }) {
-  return <>{children}</>
+  return <div className={`${caveat.className}`}>
+    {children}
+  </div>
 }
